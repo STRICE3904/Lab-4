cf7c640b42a8121df901ee00e51bf805d9855f72
STRICE3904 <trice4@tritons.iowacentral.edu>
2021-10-04 10:50:55 -0500

    Nevermind now im just summoning satan.

--- Lab-4/Source.cpp
+++ Lab-4/Source.cpp
@@ -14,7 +14,7 @@ ifstream inFile;
 
 int main()
 {
-	
+
 	tempread(inFile, inputFileName);
 	inFile.close();
 
@@ -24,47 +24,52 @@ int main()
 void tempread(ifstream& inFile, string inputFileName)
 {
 	inFile.open("tempurature.dat");
-	char inChar;
-	inFile.get(inChar);
-	float intemp = float(inChar);
+	char intemp;
+	inFile.get(intemp);
 	int mintemp = '-35';
 	int maxtemp = '120';
 	cout << "Tempurature for 24 hours:\n";
 	cout << "   -30        0        30        60        90       120";
-	string star; 
-	star = ""; 
-	while (intemp != '\0')
+	string star;
+	star = "";
+	while (intemp != 'n')
 	{
 		star.append("*");
 
-		if (intemp == -30) {
-			cout << setw(12) << star + "|";
+		if (intemp <= -35) {
+			intemp == mintemp;
+			cout << mintemp << setw(12) << star + "|";
 		}
 		else if (intemp > -30 && intemp <= -10)
 		{
-			cout << setw(12) << star + "|";
+			cout << intemp << setw(12) << star + "|";
 		}
 		else if (intemp > -10 && intemp < 0)
 		{
-			cout << setw(13) << star + "|";
+			cout << intemp << setw(13) << star + "|";
 		}
 		else if (intemp >= 0 && intemp < 10)
 		{
-			cout << setw(14) << "|" << star;
+			cout << intemp << setw(14) << "|" << star;
 		}
 		else if (intemp >= 10 && intemp < 100)
 		{
-			cout << setw(13) << "|" << star;
+			cout << intemp << setw(13) << "|" << star;
 		}
 		else if (intemp >= 100 && intemp <= 120)
 		{
-			cout << setw(12) << "|" << star;
+			cout << intemp << setw(12) << "|" << star;
+		}
+		else if (intemp >= 120)
+		{
+			intemp == maxtemp;
+			cout << maxtemp << setw(12) << "|" << star;
 		}
 
 		cout << endl;
-		inFile.get(inChar);
+		inFile.get(intemp);
 	}
-cout << "###############\n";
-cout << "* = 3 degrees";
-cout << "\n###############";
+	cout << "###############\n";
+	cout << "* = 3 degrees";
+	cout << "\n###############";
 }
